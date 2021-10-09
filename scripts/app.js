c5e6a22eb7c57787562dcfdb23897c16bd29f8dd
shahrukh-hughes <s.khan_1987@hotmail.co.uk>
2021-10-09 14:55:34 +0100

    add player function

--- scripts/app.js
+++ scripts/app.js
@@ -23,8 +23,28 @@ cells.forEach((cell, i) => {
 })
 
 //have player appear on random cell in bottom row
+// const allCellsArray = Array.from(cells)
+// console.log(allCellsArray)
+
+// const bottomRow = allCellsArray.slice(90, 99).fill('grid div')
+// console.log(bottomRow)
+
+// const randomIndex = Math.floor(Math.random()*bottomRow.length)
+// const randomisedPlayerCell = bottomRow[randomIndex]
+// console.log(randomisedPlayerCell)
+
+// cells.forEach((cell) => {
+//   cell.classList.add(bottomRow[randomIndex])
+// })
+
+// start player on bottom row
 const allCellsArray = Array.from(cells)
 console.log(allCellsArray)
 
-const bottomRow = allCellsArray.slice(90, 99).fill('grid div')
-console.log(bottomRow)
+const playerIndex = allCellsArray[94]
+console.log(playerIndex)
+
+const addPlayer = function() {
+  return playerIndex.classList.add('player')
+}
+addPlayer()
