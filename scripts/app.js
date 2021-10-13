2cf0bbe871e62846c49934ba8cd77a5d34a46621
shahrukh-hughes <s.khan_1987@hotmail.co.uk>
2021-10-13 17:43:42 +0100

    improved move aliens function so now moves left right and down and edited layout of code

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,12 @@
 
 // variable needed to start/reset game
 let hasGameStarted = false
+let movingRight = true
+let direction = 1
+let alienId
+let results = document.querySelector('#score')
+let lives = document.querySelector('#lives')
+
 
 // have random amount of aliens appear on first 3 rows
 let cells = document.querySelectorAll('.grid div')
@@ -9,86 +15,74 @@ let cells = document.querySelectorAll('.grid div')
 const allCellsArray = Array.from(cells)
 // console.log("allCellsArray", allCellsArray)
 
-const startingRowsOfNodes = Array.from(cells).slice(0, 29)
+// const startingRowsOfNodes = Array.from(cells).slice(0, 29)
 // console.log('startingRowOfNodes.length', startingRowsOfNodes.length)
-const startingRows = allCellsArray.slice(0, 29)
+// const startingRows = allCellsArray.slice(0, 29)
 // console.log('startingRows', startingRows)
-const remainingRowsOfNodes = Array.from(cells).slice(28,99)
+// const remainingRowsOfNodes = Array.from(cells).slice(28,99)
 
 // const fullGridOfNodesWithRandomAlienClasses = startingRowsOfNodes.concat(remainingRowsOfNodes)
 // console.log('fullGridOfNodesWithRandomAlienClasses', fullGridOfNodesWithRandomAlienClasses)
 
-const remainingRows = allCellsArray.slice(28, 99)
+// const remainingRows = allCellsArray.slice(28, 99)
 // console.log('remainingRows', remainingRows)
 
-// const addAliens = () => {
-//   for(i=0; i<29; i++) {
-//   if (Math.random() > 0.5) {
-//     allCellsArray[i].classList.add('alien')
-    
-//   // } else {
-//   //   startingRowsOfNodes[i].classList.add('empty')
-//   //   console.log(startingRowsOfNodes)
-//    }
+// const randomisedAliens = () => {
+//   if (Math.random() > 0.5){
+//     return 'alien'
+//   }
 // }
-// }
-const randomisedAliens = () => {
-  if (Math.random() > 0.5){
-    return 'alien'
-  }
-}
 
 // console.log('randomisedAliens', randomisedAliens())
 
-const firstAliens = startingRows.map(randomisedAliens)
+// const firstAliens = startingRows.map(randomisedAliens)
 // console.log('firstAliens', firstAliens)
 
-let gridMap = firstAliens.concat(remainingRows)
-console.log('gridMap', gridMap)
+// let gridMap = firstAliens.concat(remainingRows)
+// console.log('gridMap', gridMap)
 
 // const addAliens = () => {
-// return cells.forEach((cell, i) => {
-// cell.classList.add(gridMap[i])
-  
-// })
-// }
+//   for(let i=0; i<gridMap.length; i++) {
+//     if(gridMap[i]==='alien')
+//     allCellsArray[i].classList.add('alien')
+//   }
+//   }
+const aliens = [0,1,2,3,4,5,6,7,10,11,12,13,14,15,16,17,20,21,22,23,24,25,26,27]
 
 const addAliens = () => {
-  for(let i=0; i<gridMap.length; i++) {
-    if(gridMap[i]==='alien')
-    allCellsArray[i].classList.add('alien')
+  for(let i = 0; i < aliens.length; i++) {
+    
+    // console.log('allCellsArray', allCellsArray)
+    allCellsArray[aliens[i]].classList.add('alien')
   }
-  }
-  
-
-//start game function
-
-const start = document.querySelector('#start')
-
-const startGame = () => {
-  addAliens()
-  // console.log('allCellsArray', allCellsArray)
-  // console.log('cells', cells)
-  addPlayer()
-  moveAliens()
 }
 
-start.addEventListener('click', startGame)
 
-//have player appear on random cell in bottom row
+const removeAliens = () => {
+  for (let i = 0; i < aliens.length; i++) {
+    // console.log('alien[i]', aliens[i])
+    // console.log('allCellsArray', allCellsArray)
+    allCellsArray[aliens[i]].classList.remove('alien')
+  }
+}
+
+//add player to random cell in bottom row
+const playerStartingCell = cells[(Math.floor(Math.random()*10))+90]
+const addPlayer = () => {
+  playerStartingCell.classList.add('player')
+}
+
 
 // const bottomRow = gridMap.slice(90, 99)
 // console.log('bottowRow', bottomRow)
 
-const randomIndex = (Math.floor(Math.random()*10))+90
+// const randomIndex = 
 // console.log('randomIndex', randomIndex)
 
-const playerStartingCell = cells[randomIndex]
+
 // console.log('playerStartingCell', playerStartingCell)
 
-const addPlayer = () => {
-  return playerStartingCell.classList.add('player')
-}
+
 
 //move player
 
@@ -128,92 +122,31 @@ document.addEventListener('keydown', function (event) {
   }
 })
 
-//initalise bullet
-
-const addBullet = () => {
-  // console.log('cells[playerIndex-10]', cells[playerIndex-10])
-  return cells[playerIndex-10].classList.add('bullet')
-}
-
-//fire bullet
-
-// const bulletIndex = gridMap[playerIndex-10]
-let intervalId
-const fireBullet = () => {
-  intervalId = setInterval(() => {
-  // console.log('bullet')
-  Array.from(cells).map(cell => {
-    // console.log('cell', cell)
-    if (cell.classList.contains('bullet')) {
-      let index = Array.from(cells).indexOf(cell)
-      console.log('index', index)
-      if (index<10){
-        cells[index].classList.remove('bullet')
-        clearInterval(intervalId)
-      } else if (cells[index-10].classList.contains('alien')){
-        cells[index - 10].classList.remove('alien')
-        cells[index].classList.remove('bullet')
-        clearInterval(intervalId)
-      } else {
-      cells[index - 10].classList.add('bullet')
-      cells[index].classList.remove('bullet')
-      } 
-    } 
-    }
-  ) 
-}, 500) 
-}
-
-document.addEventListener('keydown', function (event) {
-  switch (event.key) {
-    case 'f':
-      console.log('pressed f key')
-      addBullet()
-      fireBullet()
-      break
-  }
-})
-
 // move aliens
 
-// let alienIntervalId
-
 // const moveAliens = () => {
-//    allCellsArray.map(cell => {
-//      if(cell.classList.contains('alien')) {
-//       let index = allCellsArray.indexOf(cell)
-//       console.log(allCellsArray[index])
-//       console.log('index', index)
-//       console.log('index+10', index + 10)
-//       if(index % 10 === 0){
-
-//         moveDown()
-//       }
-//     }
-//    })
-  //   Array.from(cells).map(cell => {
+  
+//   allCellsArray.map(cell => {
     
-  //   if (cell.classList.contains('alien')) {
-  //     let index = Array.from(cells).indexOf(cell)
-  //     console.log('index', index)
-  //     if (index%10===0){
-  //       console.log('index', index)
-  //       // moveDown()
-  //       clearInterval(alienIntervalId)
-  //     } else if (cells[index-1].classList.contains('alien')){
-  //       cells[index-1].classList.remove('alien')
-  //       cells[index].classList.remove('alien')
-  //       cells[index-1].classList.add('alien')
-  //     } else {
-  //     cells[index - 1].classList.add('alien')
-  //     cells[index].classList.remove('alien')
-  //     } 
-  //   } 
-  //   }
-  // ) 
+//     if (cell.classList.contains('alien')) {
+//       let index = allCellsArray.indexOf(cell)
+//       console.log('index', index)
+//       if (index%10===0){
+//         console.log('index', index)
+//         // moveDown()
+//         clearInterval(alienIntervalId)
+//       } else if (cells[index-1].classList.contains('alien')){
+//         cells[index-1].classList.remove('alien')
+//         cells[index].classList.remove('alien')
+//         cells[index-1].classList.add('alien')
+//       } else {
+//       cells[index - 1].classList.add('alien')
+//       cells[index].classList.remove('alien')
+//       } 
+//     } 
+//     }
+//   ) 
 // }
-
-// moveAliens()
 
 // const moveDown = () => {
 //   console.log('allCellsArray', allCellsArray)
@@ -227,10 +160,8 @@ document.addEventListener('keydown', function (event) {
 //        allCellsArray[index + 10].classList.add('alien')
 //      }
 //     }
-//       // 
-    
-  
-    //  console.log('move down triggered')
+//   
+//  console.log('move down triggered')
     
      
      
@@ -259,83 +190,134 @@ document.addEventListener('keydown', function (event) {
 
 // alienIntervalId = setTimeout(moveAliens, 3000)
 
-let movingLeft = true
-let direction = -1
 
-const aliensLeft = () => {
-  Array.from(cells).some((cell) => {
-      if(cell.classList.contains('aliens')){
-        return true
-      }
-  })
-}
 
-function findAliens(arr, val) {
-  let indexes = [];
-  let i=-1;
-  while((i=arr.indexOf(val, i+1)) != -1) {
-    indexes.push(i)
-  }
-  return indexes
-}
+// const aliensLeft = () => {
+//   Array.from(cells).some((cell) => {
+//       if(cell.classList.contains('aliens')){
+//         return true
+//       }
+//   })
+// }
 
-let alienIndexes = findAliens(gridMap, 'alien')
-console.log('alienIndexes', alienIndexes)
+// function findAliens(arr, val) {
+//   let indexes = [];
+//   let i=-1;
+//   while((i=arr.indexOf(val, i+1)) != -1) {
+//     indexes.push(i)
+//   }
+//   return indexes
+// }
 
-console.log('cells[alienIndexes]', allCellsArray[alienIndexes])
+// let alienIndexes = findAliens(gridMap, 'alien')
+// console.log('alienIndexes', alienIndexes)
 
-const addNextAliens = () => {
-  for(let i=0; i<alienIndexes; i++) {
-    allCellsArray[alienIndexes[i]].classList.add('alien')
-  }
-}
+// console.log('cells[alienIndexes]', allCellsArray[alienIndexes])
 
-const removeAliens = () => {
-  for (let i=0; 0<alienIndexes;i++) {
-    console.log('alienIndexes[i]', alienIndexes[i])
-    allCellsArray[alienIndexes[i]].classList.remove('alien')
-  }
-}
-
-const newIndexes = (array, x) => {
-  return array.map((index) => index+x)
-}
-console.log('newIndexes(alienIndexes)', newIndexes(alienIndexes, -1))
-
+// const addNextAliens = () => {
+//   for(let i=0; i<aliens.length; i++) {
+//     allCellsArray[aliens[i]].classList.add('alien')
+//   }
+// }
+// const newIndexes = (array, x) => {
+//   return array.map((index) => index+x)
+// }
+// console.log('newIndexes(alienIndexes)', newIndexes(alienIndexes, -1))
 
 
 const moveAliens = () => {
-
-  alienIntervalId = setInterval(() => {
+  
+  aliensId = setInterval(() => {
     
   
-  const leftEdge = alienIndexes[0] % 10 === 0
-  const rightEdge = alienIndexes[alienIndexes.length-1] % 9 === 0
+  const leftEdge = aliens[0] % 10 === 0
+  const rightEdge = aliens[aliens.length-1] % 9 === 0
   removeAliens()
 
-  if (leftEdge && movingLeft) {
-    for (let i = 0; i < alienIndexes.length; i++){
-      alienIndexes[i] += 11
-      direction = 1
-      movingLeft = false
-    }
-  }
-  if (rightEdge && !movingLeft) {
-    for (let i = 0; i < alienIndexes.length; i++) {
-      alienIndexes[i] =+ 9
+  if (rightEdge && movingRight) {
+    for (let i = 0; i < aliens.length; i++){
+      aliens[i] += 11
       direction = -1
-      movingLeft = true
+      movingRight = false
+    }
+  }
+  if (leftEdge && !movingRight) {
+    for (let i = 0; i < aliens.length; i++) {
+      aliens[i] =+ 9
+      direction = 1
+      movingRight = true
     }
   }
 
-  for(let i = 0; i < alienIndexes.length; i++) {
-    alienIndexes[i] += direction
+  for(let i = 0; i < aliens.length; i++) {
+    aliens[i] += direction
   }
 
-  addNextAliens()
+  addAliens()
 
-  
+}, 2000)
 
-}, 3000);
+ }
+
+
+
+//initalise bullet
+
+const addBullet = () => {
+  // console.log('cells[playerIndex-10]', cells[playerIndex-10])
+  return cells[playerIndex-10].classList.add('bullet')
 }
 
+//fire bullet
+
+// const bulletIndex = gridMap[playerIndex-10]
+let fireBulletId
+const fireBullet = () => {
+  fireBulletId = setInterval(() => {
+  // console.log('bullet')
+  Array.from(cells).map(cell => {
+    // console.log('cell', cell)
+    if (cell.classList.contains('bullet')) {
+      let index = Array.from(cells).indexOf(cell)
+      console.log('index', index)
+      if (index<10){
+        cells[index].classList.remove('bullet')
+        clearInterval(fireBulletId)
+      } else if (cells[index-10].classList.contains('alien')){
+        cells[index - 10].classList.remove('alien')
+        cells[index].classList.remove('bullet')
+        clearInterval(fireBulletId)
+      } else {
+      cells[index - 10].classList.add('bullet')
+      cells[index].classList.remove('bullet')
+      } 
+    } 
+    }
+  ) 
+}, 500) 
+}
+
+document.addEventListener('keydown', function (event) {
+  switch (event.key) {
+    case 'f':
+      console.log('pressed f key')
+      addBullet()
+      fireBullet()
+      break
+  }
+})
+
+//start game function
+
+const start = document.querySelector('#start')
+
+const startGame = () => {
+  addAliens()
+  // console.log('allCellsArray', allCellsArray)
+  // console.log('cells', cells)
+  addPlayer()
+  moveAliens()
+  
+}
+
+start.addEventListener('click', startGame)
